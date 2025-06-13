1743bbed3fe8bff83219e4db3a72f00e253b4b4a
Jason Abbott <woebane@gmail.com>
2025-06-13 16:56:40 -0700

    Don't make light bob so much

--- Scripts/state_intro.gd
+++ Scripts/state_intro.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 	const TEXTURE_OFFSET : float = 64 * 2
 	var frac : float = (_light_circle.position.x + TEXTURE_OFFSET - _game_label.position.x) / _game_label.size.x
 	
-	var target_y = _game_label.position.y + (cos(4.0 * frac * PI) * _game_label.size.y / 2) - _game_label.size.y / 4
+	var target_y = _game_label.position.y + (cos(4.0 * frac * PI) * _game_label.size.y / 4) - _game_label.size.y / 4
 	if _falling:
 		_light_circle.position.y += delta * SPEED
 		if _light_circle.position.y + TEXTURE_OFFSET > target_y:
