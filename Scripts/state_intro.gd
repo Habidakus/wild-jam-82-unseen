f57fbf335c91ad6aa6f6e01da70b42e397ce8099
Jason Abbott <woebane@gmail.com>
2025-06-13 18:38:18 -0700

    Moved maps and tilesets to their own folders, added fade out to title screen

--- Scripts/state_intro.gd
+++ Scripts/state_intro.gd
@@ -5,6 +5,8 @@ extends StateMachineState
 const SPEED : float = 300
 var _forward : bool = true
 var _falling : bool = true
+var _leave_tween : Tween = null
+var _can_leave : bool = false
 
 func _ready() -> void:
 	pass
@@ -25,8 +27,45 @@ func _process(delta: float) -> void:
 		_light_circle.position.x += delta * SPEED
 		if _light_circle.position.x + TEXTURE_OFFSET > _game_label.position.x + _game_label.size.x:
 			_forward = false
+			_can_leave = true
 		return
 	
 	_light_circle.position.x -= delta * SPEED
 	if _light_circle.position.x + TEXTURE_OFFSET < _game_label.position.x:
 		_forward = true
+
+func _input(event : InputEvent) -> void:
+	_handle_event(event)
+
+func _unhandled_input(event : InputEvent) -> void:
+	_handle_event(event)
+
+func _handle_event(event : InputEvent) -> void:
+	# We process on "released" instead of pressed because otherwise immediately
+	# switching screens could still have the mouse being pressed on some other
+	# screen's button.
+	if process_mode == ProcessMode.PROCESS_MODE_DISABLED:
+		return
+
+	if _can_leave == false:
+		return;
+		
+	if _leave_tween == null:
+		if event.is_released():
+			if event is InputEventKey:
+				our_state_machine.switch_state("ChapterOne")
+			if event is InputEventMouseButton:
+				our_state_machine.switch_state("ChapterOne")
+
+func exit_state(next_state: StateMachineState) -> void:
+	if _leave_tween != null && _leave_tween.is_running():
+		return
+
+	_leave_tween = get_tree().create_tween()
+	_leave_tween.tween_property(_light_circle.get_child(0), "scale", Vector2.ZERO, 1)
+	var when_finished_callback : Callable = Callable(self, "_on_leave_tween_finished")
+	_leave_tween.tween_callback(when_finished_callback.bind(next_state))
+
+func _on_leave_tween_finished(next_state: StateMachineState) -> void:
+	super.exit_state(next_state)
+	_leave_tween = null
