1d19ab95d3b29b7d3c039498bdfdc360155ed963
Jason Abbott <woebane@gmail.com>
2025-06-17 11:51:39 -0700

    Code cleanup of indent

--- addons/statemachine/StateMachine.gd
+++ addons/statemachine/StateMachine.gd
@@ -16,35 +16,35 @@ var all_states : Array = []
 var current_state : StateMachineState = null
 
 func _ready() -> void:
-	for child in get_children():
-		if child is StateMachineState:
-			_register_state(child)
-	_switch_state_internal(initial_state)
+    for child in get_children():
+        if child is StateMachineState:
+            _register_state(child)
+    _switch_state_internal(initial_state)
 
 func _register_state(state: StateMachineState) -> void:
-	all_states.append(state)
-	state.init_state(self)
+    all_states.append(state)
+    state.init_state(self)
 
 func _begin_state(state: StateMachineState) -> void:
-	current_state = state
-	if state != null:
-		state.enter_state()
+    current_state = state
+    if state != null:
+        state.enter_state()
 
 func _switch_state_internal(state: StateMachineState) -> void:
-	if current_state == state:
-		print("Already in state " + state.name + ", aborting state switch")
-		return
-	if current_state != null:
-		var callback : Callable = Callable(self, "_begin_state")
-		current_state._set_completion_call(callback.bind(state))
-		current_state.exit_state(state)
-	else:
-		_begin_state(state)
+    if current_state == state:
+        print("Already in state " + state.name + ", aborting state switch")
+        return
+    if current_state != null:
+        var callback : Callable = Callable(self, "_begin_state")
+        current_state._set_completion_call(callback.bind(state))
+        current_state.exit_state(state)
+    else:
+        _begin_state(state)
 
 ## Called when the state machine should transition to another state
 func switch_state(next_state_name: String) -> void:
-	for state : StateMachineState in all_states:
-		if state.name == next_state_name:
-			_switch_state_internal(state)
-			return
-	assert(false, "{0} is not a valid state".format([next_state_name]))
+    for state : StateMachineState in all_states:
+        if state.name == next_state_name:
+            _switch_state_internal(state)
+            return
+    assert(false, "{0} is not a valid state".format([next_state_name]))
