46b6efb0a9a76da6b16f977115d43c334096002e
Jason Abbott <woebane@gmail.com>
2025-06-19 09:38:19 -0700

    Added initial test of a teleportation zone

---
+++ Scripts/teleportation_area.gd
@@ -0,0 +1,26 @@
+class_name TeleportationArea extends Node2D
+
+## Drag the map (State Machine State) here which the player should be moved to
+@export var destination_map : StateMachineState
+
+# Having the state machine set to non-null is a sentinel value that triggers the switching
+# of the states outside of the physics loop (which angers the Godot engine)
+var _our_state_machine : StateMachine = null
+
+func _process(_delta: float) -> void:
+    if _our_state_machine != null:
+        _our_state_machine.switch_state(destination_map.name)
+        _our_state_machine = null
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+    if not body is Player:
+        return
+    
+    var node : Node = self
+    while not node is StateMachineState:
+        node = node.get_parent()
+        if node == null:
+            print("%s is not a child of a state machine state! Cannot teleport" % self.name)
+            return
+     
+    _our_state_machine = (node as StateMachineState).our_state_machine
