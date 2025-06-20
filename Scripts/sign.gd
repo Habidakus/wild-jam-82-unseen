4df40c6e575a5afaa5bbd9d404a0c98a241dbafe
Jason Abbott <woebane@gmail.com>
2025-06-20 15:05:25 -0700

    Signs use better artwork, and use the Scroll Layer

--- Scripts/sign.gd
+++
@@ -1,7 +0,0 @@
-class_name TooltipSign extends Node2D
-
-## What text the sign should display when the mouse hovers over it
-@export var sign_text : String
-
-func _ready() -> void:
-    (find_child("Control") as Control).tooltip_text = sign_text
