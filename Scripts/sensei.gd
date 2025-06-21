737e1277f858b3ac40f05f76f7efaf7497131fca
Jason Abbott <woebane@gmail.com>
2025-06-21 10:23:41 -0700

    Attend me, my dude.

--- Scripts/sensei.gd
+++ Scripts/sensei.gd
@@ -45,9 +45,9 @@ func remove_tier_block(tier : int) -> void:
 
 func send_intro_dialog() -> void:
 	var labels : Array[Control] = []
+	labels.append(generate_label("Attend me, my dude."))
 	labels.append(generate_label("You have come to me to master ninja fishing."))
 	labels.append(generate_label("I am California Sensei, and I am hella good at ninja fishing."))
-	
 	var report_card : ReportCard = _map_runner.get_report_card()
 	var direct_player_to_pond : bool = true
 	var tell_players_to_fish : bool = true
@@ -57,7 +57,7 @@ func send_intro_dialog() -> void:
 		if report_card.what_tier_does_report_unlock() == 1:
 			tell_players_to_fish = false
 	if direct_player_to_pond:
-			labels.append(generate_label("Attend me, my dude.\nWalk to my pond just to the East."))
+			labels.append(generate_label("Walk to my pond just to the East."))
 	if tell_players_to_fish:
 		labels.append(generate_label("Return when you have caught two fish."))
 	_scroll_layer.display_series(labels, _map_runner._player)
